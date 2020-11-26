c38fec08d585f149d4a5f5e41ea3be34268fea2e
Purplette <47246743+Purplette@users.noreply.github.com>
2020-11-26 16:54:30 +0100

    rajout js affichage des parties

---
+++ miseenpage.js
@@ -0,0 +1,35 @@
+/*ACCUEIL -> CONNEXION */
+document.querySelector('#button_connexion').addEventListener('click',function(){
+    document.querySelector('#accueil').classList.remove("section-active");
+    document.querySelector('#connexion').classList.add("section-active");
+
+});
+
+/*CONNEXION -> DIAL  */
+document.querySelector('#button_connexion_form').addEventListener('click',function(){
+    document.querySelector('#connexion').classList.remove("section-active");
+    document.querySelector('#dial').classList.add("section-active");
+
+});
+
+/*DIAL -> CALL */
+document.querySelector('#button_appel').addEventListener('click',function(){
+    document.querySelector('#dial').classList.remove("section-active");
+    document.querySelector('#call').classList.add("section-active");
+
+});
+
+/*CALL -> DIAL */
+document.querySelector('#button_raccrocher').addEventListener('click',function(){
+    document.querySelector('#call').classList.remove("section-active");
+    document.querySelector('#dial').classList.add("section-active");
+
+});
+
+/*CALL -> DIAL */
+document.querySelector('#button_rediriger').addEventListener('click',function(){
+    document.querySelector('#call').classList.remove("section-active");
+    document.querySelector('#dial').classList.add("section-active");
+
+});
+
