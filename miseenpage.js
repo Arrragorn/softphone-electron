a9c2b7fabe4968bc57e8c2fdba08dd0a61df2aa4
Purplette <47246743+Purplette@users.noreply.github.com>
2020-12-03 16:17:03 +0100

    systeme dial, css header,fonction changement header

--- miseenpage.js
+++ miseenpage.js
@@ -1,65 +1,96 @@
 /*ACCUEIL -> CONNEXION */
-document.querySelector('#button_connexion').addEventListener('click',function(){
-    document.querySelector('#accueil').classList.remove("section-active");
-    document.querySelector('#connexion').classList.add("section-active");
+$('#button_connexion').click(function(){
+    $('#accueil').removeClass('section-active');
+    $('#connexion').addClass('section-active');
 
 });
 
 /*CONNEXION -> DIAL  */
-document.querySelector('#button_connexion_form').addEventListener('click',function(){
-    document.querySelector('#connexion').classList.remove("section-active");
-    document.querySelector('#dial').classList.add("section-active");
+$('#button_connexion_form').click(function(){
+    $('#connexion').removeClass('section-active');
+    $('#dial').addClass('section-active');
 
 });
 
 /*DIAL -> CALL */
-document.querySelector('#button_appel').addEventListener('click',function(){
-    document.querySelector('#dial').classList.remove("section-active");
-    document.querySelector('#call').classList.add("section-active");
+$('#button_appel').click(function(){
+    $('#dial').removeClass('section-active');
+    $('#call').addClass('section-active');
 
 });
 
 /*CALL -> DIAL */
-document.querySelector('#button_raccrocher').addEventListener('click',function(){
-    document.querySelector('#call').classList.remove("section-active");
-    document.querySelector('#dial').classList.add("section-active");
+$('#button_raccrocher').click(function(){
+    $('#call').removeClass('section-active');
+    $('#dial').addClass('section-active');
 
 });
 
 /*CALL -> DIAL */
-document.querySelector('#button_rediriger').addEventListener('click',function(){
-    document.querySelector('#call').classList.remove("section-active");
-    document.querySelector('#dial').classList.add("section-active");
+$('#button_rediriger').click(function(){
+    $('#call').removeClass('section-active');
+    $('#dial').addClass('section-active');
 
 });
 
 /*GESTION APPARITION FENETRE INCOMING CALL*/
 
 function incomingcall(){
-    document.querySelector('#incomingcall').classList.add("section-active");    
+    $('#incommingcall').addClass('section-active');
 }
 
 /*GESTION BOUTONS FENETRE INCOMING CALL*/
 
-document.querySelector('#button_appel2').addEventListener('click',function(){
-    document.querySelectorAll(section).classList.remove("section-active");
-    document.querySelector('#call').classList.add("section-active");
+$('#button_appel2').click(function(){
+    $('section').removeClass('section-active');
+    $('#call').addClass('section-active');
+
 });
 
-document.querySelector('#button_raccrocher2').addEventListener('click',function(){
-    document.querySelector('#incomingcall').classList.remove("section-active");
+$('#button_raccrocher2').click(function(){
+    $('#incomingcall').classList.remove("section-active");
 });
 
 
-if(document.querySelector('#').classList.contains('section-active')){
-    console.log("lel");
+/*GERER CONNEXION*/
 
-    document.querySelectorAll('.num').addEventListener('click',function(){
-    
-        var numero = this.value;
-        console.log(numero);
-       
+function connecter(){
+
+    $('.codeco').html('<span class="sedeconnecter"> -Se déconnecter</span>')
+    $('.sedeconnecter').click(function(){
+        $('section').removeClass('section-active');
+        $('#accueil').addClass('section-active');
     });
-    
+
+}
+function deconnecter(){
+
+    $('.codeco').html('<span class="seconnecter"> -Se connecter</span>')
+    $('.seconnecter').click(function(){
+           $('section').removeClass('section-active');
+        $('#connexion').addClass('section-active');
+    });
+
 };
+ 
+
+
+/*GESTION DU DIAL*/
+
+    
+    $('.num').click(function(){
+    
+        var numero =$(this).children(0).html();
+        
+        if($('#numeroRentrer').val() == "") {
+            var anciennum="";
+        }
+        else { 
+            var anciennum =String($('#numeroRentrer').val()); 
+        } 
+       
+        $('#numeroRentrer').val(anciennum+numero);
+    });
+        
+ 
 
